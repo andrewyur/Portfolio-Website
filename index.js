8923e6095ef6666972b0e7c9673d86385a19834e
Andrew Yurovchak <andy@yurovchak.net>
2023-09-01 16:26:09 -0400

    Added App (again)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Suspense } from 'react'
 import  { createRoot }  from 'react-dom/client';
-import About from './src/About.js'
+import App from './src/App.js'
 import './public/index.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
 <Suspense fallback={<div className="center">loading</div>}>
-    <About/>
+    <App/>
 </Suspense>
 );
