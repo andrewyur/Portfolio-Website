decc80fab63161dfb3556b4a14a7b1074693ab40
Andrew Yurovchak <andy@yurovchak.net>
2023-09-05 19:24:08 -0400

    added mouse movement

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, act, useFrame } from '@react-three/fiber'
 import { useGLTF, Environment, OrbitControls } from '@react-three/drei'
 
 // interactivity variable ideas:
@@ -28,25 +28,53 @@ const MOUSE_SENSITIVITY = 600;
 // add photos
 
 const Torso = function(props){
-    const torsoRef = useRef();
+    const [active, setActive] = useState(false)
+    const torsoRef = useRef()
     useFrame(() => {
-        torsoRef.current.rotation.y += 0.003;
-    }) 
+        if(!active){
+            torsoRef.current.rotation.y += 0.003;
+        }
+    })
+    useEffect(()=>{
+        document.addEventListener('mousedown', handleMouseDown)
+        document.addEventListener('mouseup', handleMouseUp)
+        document.addEventListener('mousemove', handleMouseMove)
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove)
+            document.removeEventListener('mousedown', handleMouseDown)
+            document.removeEventListener('mouseup', handleMouseUp)}
+    })
+
+    const handleMouseMove = (event) => {
+        if(active){
+            torsoRef.current.rotation.x += event.movementY/MOUSE_SENSITIVITY
+            torsoRef.current.rotation.y += event.movementX/MOUSE_SENSITIVITY
+        }
+    }
+    const handleMouseDown = () => {
+        setActive(true)
+    }
+    const handleMouseUp = () => {
+        setActive(false)
+    }
+
+    console.log(props.offsetX)
     return <primitive 
         ref={torsoRef}
         position={[0.3,-0.2,0]} 
-        rotation={[0,0,0]} 
+        rotation={[0.25, 0, 0.5]} 
         scale={0.225} 
         object={useGLTF('/4 arm torso.glb').scene}
     />
 }
 
+const environment = ENVIRONMENT_PRESETS[Math.floor(Math.random() * ENVIRONMENT_PRESETS.length)]
+const background = BACKGROUND_COLORS[Math.floor(Math.random() * BACKGROUND_COLORS.length)]
+
 export default function About() {
-    const environment = ENVIRONMENT_PRESETS[Math.floor(Math.random() * ENVIRONMENT_PRESETS.length)]
-    const background = BACKGROUND_COLORS[Math.floor(Math.random() * BACKGROUND_COLORS.length)]
-    
+    const aboutRef = useRef()
     return (
-        <div id="about" style={{backgroundColor: background}}>
+        <div id="about" style={{backgroundColor: background}} ref={aboutRef}>
                 <div id="about-text">
                     <p>I am a sophomore Computer Science student at Stevens Institute of Technology.</p>
                     <p>I know how to use Java and Python reasonably well, and am learning C++ through my school classes</p>
@@ -59,7 +87,7 @@ export default function About() {
                     <hr/>
                 </div>
                 <h1 id="about-background-text">ABOUT ME</h1>
-                <Canvas id="canvas" camera={{ position: [-2.5,1.5,4], fov: 10 }} >
+                <Canvas id="canvas" camera={{ position: [-2.5,1.5,4], fov: 10}}>
                     <ambientLight intensity={0.5} />
                     <Torso/>
                     <Environment preset={environment} />
