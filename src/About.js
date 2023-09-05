d61a0f8234f4b5e442403cef18b6c19395005008
Andrew Yurovchak <andy@yurovchak.net>
2023-09-05 14:32:29 -0400

    refactored to use react hooks, edited features

--- src/About.js
+++ src/About.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { Canvas } from '@react-three/fiber'
+import { useState, useEffect, useRef } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF, Environment, OrbitControls } from '@react-three/drei'
-
-function Torso(props) {
-    const {scene} = useGLTF('/4 arm torso.glb')
-    return <primitive object={scene} {...props} />
-}
 
 // interactivity variable ideas:
 // environment preset : apartment, city, dawn, forest, studio, sunset, 
@@ -14,7 +10,7 @@ function Torso(props) {
 // mesh rotation : drag/click
 // camera fov : find relationship between fov and scale to make mesh appear the same size each time fov is changed
 
-const ENVIRONMENT_PRESETS = ["city", "dawn", "forest", "studio", "sunset"]
+const ENVIRONMENT_PRESETS = ["city", "dawn", "forest", "sunset"]
 const BACKGROUND_COLORS = ["#cdbaba"]
 const MOUSE_SENSITIVITY = 600;
 
@@ -31,49 +27,26 @@ const MOUSE_SENSITIVITY = 600;
 // styling
 // add photos
 
-export default class About extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            xRotation: 0,
-            yRotation: 0,
-            zRotation: 0, 
-            environment: ENVIRONMENT_PRESETS[Math.floor(Math.random() * ENVIRONMENT_PRESETS.length)],
-            background: BACKGROUND_COLORS[Math.floor(Math.random() * BACKGROUND_COLORS.length)],
-            dragging: false,
-        }
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.handleMouseUp = this.handleMouseUp.bind(this);
-    }
-    handleMouseDown(event){
-        this.setState({dragging: true})
-    }
-    handleMouseMove(event){
-        if(this.state.dragging){
-            this.setState((prevstate) => ({
-                yRotation: prevstate.yRotation + event.movementX/MOUSE_SENSITIVITY,
-                xRotation: prevstate.xRotation + event.movementY/MOUSE_SENSITIVITY
-            }))
-        }
-    }
-    handleMouseUp(event){
-        this.setState({dragging: false})
-    }
-    componentDidMount(){
-        document.addEventListener('mousedown', this.handleMouseDown);
-        document.addEventListener('mousemove', this.handleMouseMove);
-        document.addEventListener('mouseup', this.handleMouseUp);
-    }
-    componentWillUnmount(){
-        document.removeEventListener('mousedown', this.handleMouseDown);
-        document.removeEventListener('mousemove', this.handleMouseMove);
-        document.removeEventListener('mouseup', this.handleMouseUp);
-    }
-    render(){
-        console.log(this.state.background);
-        return (
-            <div id="about" style={{backgroundColor: this.state.background}}>
+const Torso = function(props){
+    const torsoRef = useRef();
+    useFrame(() => {
+        torsoRef.current.rotation.y += 0.003;
+    }) 
+    return <primitive 
+        ref={torsoRef}
+        position={[0.3,-0.2,0]} 
+        rotation={[0,0,0]} 
+        scale={0.225} 
+        object={useGLTF('/4 arm torso.glb').scene}
+    />
+}
+
+export default function About() {
+    const environment = ENVIRONMENT_PRESETS[Math.floor(Math.random() * ENVIRONMENT_PRESETS.length)]
+    const background = BACKGROUND_COLORS[Math.floor(Math.random() * BACKGROUND_COLORS.length)]
+    
+    return (
+        <div id="about" style={{backgroundColor: background}}>
                 <div id="about-text">
                     <p>I am a sophomore Computer Science student at Stevens Institute of Technology.</p>
                     <p>I know how to use Java and Python reasonably well, and am learning C++ through my school classes</p>
@@ -88,10 +61,9 @@ export default class About extends React.Component {
                 <h1 id="about-background-text">ABOUT ME</h1>
                 <Canvas id="canvas" camera={{ position: [-2.5,1.5,4], fov: 10 }} >
                     <ambientLight intensity={0.5} />
-                    <Torso position={[0.3,-0.2,0]} rotation={[this.state.xRotation, this.state.yRotation, this.state.zRotation]} scale={0.225} />
-                    <Environment preset={this.state.environment} />
+                    <Torso/>
+                    <Environment preset={environment} />
                 </Canvas>
             </div>
-        )
-    }
+    )
 }
